0297c07c6b98eca6b91d8a514ae8e675e3c752a3
Yana Zaitseva <zaitsevayg@gmail.com>
2026-01-17 17:38:08 +0300

    main_page tests. product_page def's

--- src/tool_shop/pages/web_pages/product_page.py
+++ src/tool_shop/pages/web_pages/product_page.py
@@ -12,6 +12,8 @@ class ProductPage:
         self.product_name = page.locator("[data-test='product-name']")
         self.product_description = page.locator("[data-test='product-description']")
         self.co_rating = page.locator("[data-test='co2-rating-badge']")
+        self.category_badge = page.locator('span[aria-label="category"]')
+        self.brand_badge = page.locator('span[aria-label="brand"]')
 
         self.quantity = page.locator("[data-test='quantity']")
         self.increase_quantity = page.locator("[data-test='increase-quantity']")
@@ -31,6 +33,30 @@ class ProductPage:
 
     def check_product_details(self, product: Product):
         with allure.step(f"Проверка, что на странице товара отображаются верные значения и активны кнопки"):
+            expect(self.product_price).to_equal(product.price)
+            expect(self.product_name).to_equal(product.name)
+            expect(self.product_description).to_be_visible()
+            expect(self.add_to_card).not_to_be_disabled()
+            expect(self.add_to_card).to_be_enabled()
+            expect(self.add_to_favorites).not_to_be_disabled()
+            expect(self.add_to_favorites).to_be_enabled()
+            expect(self.category_badge).to_have_text(product.category)
+            expect(self.brand_badge).to_have_text(product.brand)
+
+    def check_co2_rating(self, product: Product):
+        with allure.step(f"Проверка CO2 рейтинга: {product.co2}"):
+            # Локатор активного рейтинга
+            active_span = self.page.locator(
+                '[data-test="co2-rating-badge"] .co2-letter.active'
+            )
+            # Проверки
+            expect(active_span).to_be_visible()
+            expect(active_span).to_have_text(product.co2.upper())
+            expect(active_span).to_have_class(f"rating-{product.co2.lower()}")
+            # Дополнительно: все spans существуют
+            expect(self.page.locator('[data-test="co2-rating-badge"] .co2-letter')).to_have_count(5)
+
+
 
 
 
